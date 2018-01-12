c73b5628e4a0b1de8ae2de675a07ceb8b5d8d454
Ross Fletcher <rtbfletch@outlook.com>
2018-01-12 08:40:33 -0800

    add passing getName test before getAgenda test

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,14 +4,21 @@ package models;
  * Created by Guest on 1/12/18.
  */
 public class Event {
+    private String name;
     private String agenda;
 
-    public Event (String agenda) {
+    public Event (String name, String agenda) {
+        this.name = name;
         this.agenda = agenda;
+
     }
 
     public String getAgenda() {
         return agenda;
+    }
+
+    public String getName() {
+        return name;
     }
 
 
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -11,14 +11,20 @@ public class EventTest {
 
     @Test
     public void NewEventObjectInstantiatesCorrectly_true() throws Exception {
-        Event event = new Event("Coding and Hackathon Event");
+        Event event = new Event("Java Lecture", "Classes and Objects in Java");
         assertEquals(true, event instanceof Event);
     }
 
     @Test
     public void EventInstantiatesWithAgenda_true() throws Exception {
-        Event event = new Event("Coding and Hackathon Event");
-        assertEquals("Coding and Hackathon Event", event.getAgenda());
+        Event event = new Event("Java Lecture", "Classes and Objects in Java");
+        assertEquals("Java Lecture", event.getName());
+    }
+
+    @Test
+    public void EventInstantiatesWithEventName_true() throws Exception {
+        Event event = new Event("Java Lecture", "Classes and Objects in Java");
+        assertEquals("Classes and Objects in Java", event.getAgenda());
     }
 
 }
