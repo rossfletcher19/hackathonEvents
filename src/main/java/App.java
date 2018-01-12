4833fd841440354fa943febd652c100e8a029a2a
Ross Fletcher <rtbfletch@outlook.com>
2018-01-12 10:42:52 -0800

    update app with proper route to event form and success hbs files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,26 +11,30 @@ public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
 
-            get("/", (request, response) -> {
-                Map<String, Object> model = new HashMap<String, Object>();
+            get("/", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                ArrayList<Event> events = Event.getAll();
+                model.put("events", events);
                 return new ModelAndView(model, "index.hbs");
             }, new HandlebarsTemplateEngine());
 
+            get("/events/new", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return new ModelAndView(model, "event-form.hbs");
+            }, new HandlebarsTemplateEngine());
+
             post("/events/new", (request, response) -> { //URL to make new post on POST route
-                Map<String, Object> model = new HashMap<String, Object>();
+                Map<String, Object> model = new HashMap<>();
                 String name = request.queryParams("name");
                 String agenda = request.queryParams("agenda");
                 Event newEvent = new Event(name, agenda);
+                model.put("EventName", newEvent);
                 return new ModelAndView(model, "success.hbs");
             }, new HandlebarsTemplateEngine());
 
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Event> events = Event.getAll();
-            model.put("events", events);
 
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+
+
 
 
         }
